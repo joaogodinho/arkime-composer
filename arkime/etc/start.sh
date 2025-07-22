cdcbe976372848b95c0b90a35d4e616e6e9df287
joaogodinho <joaogodinho@users.noreply.github.com>
2025-07-22 14:59:16 +0100

    Feat: Add healthchecks

--- arkime/etc/start.sh
+++ arkime/etc/start.sh
@@ -4,9 +4,9 @@ echo "Giving ES time to start..."
 sleep 10
 
 if [ ! -f /initialized ]; then
-    touch /initialized
     /opt/arkime/db/db.pl elasticsearch:9200 init
     /opt/arkime/bin/arkime_add_user.sh admin "Admin" admin --admin
+    touch /initialized
 fi
 
 /opt/arkime/bin/docker.sh viewer -n node00
